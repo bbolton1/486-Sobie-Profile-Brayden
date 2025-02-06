2fafdf4cff8d78ae1df0c7ac4bbba7c17f97fe32
Bolton <bbolton1@una.edu>
2025-02-06 17:15:09 -0600

    Ain't that just fantastic

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser')
+const { ObjectId } = require('mongodb')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
 
@@ -66,11 +67,69 @@ app.get('/read', async function (req,res) {
 
 // begin all middleware
 
+app.get('/insert', async (req,res)=> {
+
+
+  console.log('in /insert');
+  
+  //let newSong = req.body.myName; //only for POST, GET is req.params? 
+
+  let newSong = req.query.myName;
+
+  //connect to db,
+  await client.connect();
+  //point to the collection 
+  await client
+    .db("rock-song-database")
+    .collection("rock-song-names")
+    .insertOne({ song_name: "hi"});
+
+  res.redirect('/read');
+
+}); 
+
 app.get('/', function (req, res) {
   res.sendFile('index.html')
 })
 
 //function below also works with app.post by changing method on form to post and changing the line to console.log(req.body) 
+
+app.post('/delete/:id', async (req,res)=>{
+
+  console.log("in delete, req.parms.id: ", req.params.id)
+
+  client.connect; 
+  const collection = client.db("rock-song-database").collection
+  ("rock-song-names");
+  let result = await collection.findOneAndDelete( 
+    {
+      "_id": new ObjectId(req.params.id)
+    }
+  )
+  
+    .then(result => {
+  console.log(result); 
+  res.redirect('/read');})
+
+  
+
+})
+
+app.post('/update', async (req,res)=>{
+
+  console.log("req.body: ", req.body)
+
+  client.connect; 
+  const collection = client.db("rock-song-database").collection("rock-song-names");
+  let result = await collection.findOneAndUpdate( 
+  {"_id": new ObjectId(req.body.nameID)}, 
+  { $set: {song_name: "asdf"}}
+)
+.then(result => {
+  console.log(result); 
+  res.redirect('/read');
+})
+}); 
 
 app.get('/saveMyNameGet', (req,res)=>{
   console.log("did we hit the end point?");
