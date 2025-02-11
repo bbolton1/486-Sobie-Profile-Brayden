5066fb25d302c51f1645cf9425d324d7da29f7e3
Bolton <bbolton1@una.edu>
2025-02-11 16:21:25 -0600

    get to work

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// MONGO_URI = "mongodb+srv://Brayden:<password>@cluster1.tr3ov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
+
 const express = require('express')
 require('dotenv').config()
 const app = express()
@@ -7,12 +9,9 @@ const { ObjectId } = require('mongodb')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
 
-console.log(uri)
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'))
-
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,46 +22,32 @@ const client = new MongoClient(uri, {
   }
 });
 
-//console.log(shajs('sha256').update())
+const mongoCollection = client
+.db("braydenSobieDb")
+.collection("sobieDbColl");
+// .db("rock-song-database")
+// .collection("rock-song-names");
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-//run().catch(console.dir);
+function initProfileData() {
 
-async function getData() {
+  mongoCollection.insertOne({
 
-  await client.connect();
-  let collection = await client.db("rock-song-database").collection("rock-song-names");
-
-  //let collection = await db.collection("posts");
-  let results = await collection.find({}).toArray();
-    //.limit(50)
-    //.toArray();
-
-  console.log(results);
-
-  return results;
+    title: "page title",
+    post: "this is the post"
+  });
 
 }
 
-app.get('/read', async function (req,res) {
-  let getDataResults = await getData();
-  console.log(getDataResults);
+//initProfileData();
 
-  res.render('songs',
-    {songData : getDataResults});
 
-  //res.send(getDataResults);
+app.get('/', async function (req,res) {
+
+  let results = await mongoCollection.find({}).toArray();
+
+  res.render('profile',
+    {profileData: results});
+
 })
 
 // begin all middleware
@@ -130,43 +115,6 @@ app.post('/update', async (req,res)=>{
   res.redirect('/read');
 })
 }); 
-
-app.get('/saveMyNameGet', (req,res)=>{
-  console.log("did we hit the end point?");
-
-  console.log('req.query: ', req.query);
-
-  let reqName = req.query.myNameGet;
-
-  //res.redirect('/ejs');
-
-  res.render('example', 
-    {pageTitle: reqName}
-  );
-})
-
-app.post('/saveMyName', (req,res)=>{
-  console.log("did we hit the end point?");
-
-  console.log(req.body);
-
-  //res.render('example',
-  //{pageTitle: reqName});
-
-})
-
-app.get('/nodemon', function (req, res) {
-  res.send('heheheha. grrr')
-})
-
-app.get('/ejs', function (req, res)
-  {res.render('example', 
-    {pageTitle: 'hello'})
-});
-
-app.get('/helloRender', function (req, res) {
-  res.send('Hello Express from the real world<br><a href="/">back to home</a>')
-})
 
 app.listen(
   port,
